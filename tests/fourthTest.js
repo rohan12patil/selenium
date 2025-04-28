a14acf97016feb2e8de4940701cf46bb53260605
Rohan Patil <rohan12patil@gmail.com>
2025-04-28 10:06:50 +0100

    feat: enhance fourth test by adding checkbox selection and dropdown interaction

--- tests/fourthTest.js
+++ tests/fourthTest.js
@@ -11,11 +11,36 @@ describe('Fourth Test', function () {
     // Navigate to the app
     await driver.get(URL);
 
+    // Checkbox
+    let checkbox = await driver.findElement(By.id('my-check-1'));
+
+    // checkbox.isSelected().then(function (value) {
+    //   if (value === true) {
+    //     console.log('Checkbox is selected');
+    //   } else {
+    //     console.log('Checkbox is not selected');
+    //   }
+    // });
+
+    let isSelected = checkbox.isSelected();
+    if (!isSelected) {
+      checkbox.click();
+    }
+
+    // dropDown Menu
+    let dropDown = await driver.findElement(By.name('my-select'));
+
+    // Get all the options within the dropdown
+    let options = await dropDown.findElements(By.css('option'));
+
+    // Select the option by index (e.g., index 1)
+    await options[1].click();
+
     // Click Link
     let link = await driver.findElement(
       By.xpath("//a[normalize-space()='Return to index']")
     );
-    link.click();
+    // link.click();
 
     // Close the browser
     // driver.quit();
