6f52acc2e0b7f3d6235f06267d20d730bc56303f
Rohan Patil <rohan12patil@gmail.com>
2025-04-28 10:47:08 +0100

    Explicit Wait

--- tests/wait.js
+++ tests/wait.js
@@ -1,16 +1,32 @@
-import { Builder, By, Key } from 'selenium-webdriver';
+import { Builder, By, Key, until } from 'selenium-webdriver';
 import * as chai from 'chai';
 const should = chai.should();
 
 const URL = 'https://www.selenium.dev/selenium/web/dynamic.html';
 
 describe('Wait Test', function () {
-  it('Should wait for the element to be visible', async function () {
+  let driver;
+  before(async function () {
+    driver = await new Builder().forBrowser('chrome').build();
+  });
+
+  xit('Implicit Wait : Should wait for the element to be visible', async function () {
     // Implicit Wait
-    let driver = await new Builder().forBrowser('chrome').build();
     await driver.manage().setTimeouts({ implicit: 1000 });
     await driver.get(URL);
     await driver.findElement(By.id('reveal')).click();
     await driver.findElement(By.id('revealed')).sendKeys('Hello World');
+  });
+
+  it('Explicit Wait : Should wait for the element to be visible', async function () {
+    // Explicit Wait
+    await driver.get(URL);
+    await driver.findElement(By.id('reveal')).click();
+
+    let revealed = await driver.findElement(By.id('revealed'));
+    const element = await driver.wait(until.elementIsVisible(revealed), 2000);
+
+    await driver.wait(until.elementIsVisible(element), 10000);
+    await element.sendKeys('Explicit Wait Hi World');
   });
 });
